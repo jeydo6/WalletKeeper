01a544de8e51e6668be1789918d47dd021406ea7
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-05-09 00:55:18 +0300

    create UsersController (#1)

---
+++ WalletKeeper.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WalletKeeper.Application.Dto;
+using WalletKeeper.Domain.Entities;
+using WalletKeeper.Domain.Exceptions;
+using WalletKeeper.Persistence.DbContexts;
+
+namespace WalletKeeper.WebAPI.Controllers
+{
+	public class UsersController : ControllerBase
+	{
+		private readonly ApplicationDbContext _dbContext;
+		private readonly ILogger<UsersController> _logger;
+
+		public UsersController(
+			ApplicationDbContext dbContext,
+			ILogger<UsersController> logger
+		)
+		{
+			_dbContext = dbContext;
+			_logger = logger;
+		}
+
+
+	}
+}
