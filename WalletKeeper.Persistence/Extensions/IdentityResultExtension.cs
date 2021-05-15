be56421f03ed3aba6fafe45e90114ff53b975e1d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-05-16 01:20:18 +0300

    fix the extension visibility;

--- WalletKeeper.Persistence/Extensions/IdentityResultExtension.cs
+++ WalletKeeper.Persistence/Extensions/IdentityResultExtension.cs
@@ -6,7 +6,7 @@ using WalletKeeper.Domain.Exceptions;
 
 namespace WalletKeeper.Persistence.Extensions
 {
-	public static class IdentityResultExtension
+	internal static class IdentityResultExtension
 	{
 		public static void EnsureSuccess(this IdentityResult identityResult, String errorMessage, ILogger logger)
 		{
