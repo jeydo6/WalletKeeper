c1e46c90ce9ae509bb45b1795b03a7c123cb423f
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-05-19 11:23:33 +0300

    #14 - implement UserClaimsPrincipalFactory;

--- WalletKeeper.Demo/Factories/UserClaimsPrincipalFactory.cs
+++ WalletKeeper.Demo/Factories/UserClaimsPrincipalFactory.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using WalletKeeper.Demo.DataContexts;
 using WalletKeeper.Domain.Entities;
+using WalletKeeper.Domain.Exceptions;
 
 namespace WalletKeeper.Demo.Factories
 {
@@ -25,9 +27,35 @@ namespace WalletKeeper.Demo.Factories
 
 		public async Task<ClaimsPrincipal> CreateAsync(User user)
 		{
-			var claimsPrincipal = new ClaimsPrincipal();
+			if (user == null)
+			{
+				throw new BusinessException("User is not exists!");
+			}
+
+			var claimsIdentity = GenerateClaimsIdentity(user);
+			var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
 
 			return await Task.FromResult(claimsPrincipal);
+		}
+
+		private ClaimsIdentity GenerateClaimsIdentity(User user)
+		{
+			var identity = new ClaimsIdentity("Identity.Application", ClaimTypes.Name, ClaimTypes.Role);
+
+			identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+			identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+			identity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+
+			var roles = _dataContext.UserRoles
+				.Where(ur => ur.UserId == user.Id)
+				.Join(_dataContext.Roles, ur => ur.RoleId, r => r.Id, (ur, r) => r)
+				.Distinct();
+			foreach(var role in roles)
+			{
+				identity.AddClaim(new Claim(ClaimTypes.Role, role.Name));
+			}
+
+			return identity;
 		}
 	}
 }
